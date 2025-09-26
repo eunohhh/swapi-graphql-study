e8d892386879df45d415d4ba21083287d3928bc3
EunOh <bdohhhhh@gmail.com>
2025-09-26 22:49:03 +0900

    update: add PreloadQuery at page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Suspense } from "react";
+import { GET_ALL_FILMS } from "@/graphql/queries";
+import { PreloadQuery } from "@/lib/apollo-client";
+import MainTemplate from "@/templates/main-template";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <PreloadQuery query={GET_ALL_FILMS}>
+      <Suspense fallback={<div>loading</div>}>
+        <MainTemplate />
+      </Suspense>
+    </PreloadQuery>
   );
 }
