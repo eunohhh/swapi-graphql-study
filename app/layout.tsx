70eeb4179a350bc17a554a313f072d373c69e7d2
EunOh <bdohhhhh@gmail.com>
2025-09-26 21:15:54 +0900

    script: tanstack -> apollo 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import QueryProvider from "@/lib/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryProvider>{children}</QueryProvider>
+        {children}
         <Toaster richColors position="top-center" />
       </body>
     </html>
