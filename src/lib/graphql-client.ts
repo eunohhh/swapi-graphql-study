02f60d6657f6be5deae3610892c69d964eea5756
EunOh <bdohhhhh@gmail.com>
2025-09-26 21:23:15 +0900

    add: apollo client

--- src/lib/graphql-client.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-export const graphqlClient = new GraphQLClient("./schema.graphql", {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
